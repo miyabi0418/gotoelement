66d9a365c8aee42483e71a4fb76cc35c164f90c3
miyabi0418 <amy3850272@gmail.com>
2020-03-22 14:42:52 +0800

    讀我擋

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # gotoelement
 去到指定元素(back to top /下錨點捲動)
+
+#範例
+
+#CDN
+
+使用說明 jQuery CDN
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+gotoelement CDN
+
+    <script src=""></script>
+
+#添加說明
+
+    data-goto-target="#指定元素的 ID"
+    data-goto-duration="時間"
---
+++ goToElement.js
@@ -0,0 +1,25 @@
+$("*").each(function () {
+    $(this).click(function () {
+        var target = $(this).attr("data-goto-target");
+
+        // 如果 目標屬性 不等於 未定義資料
+        if (target != undefined) {
+
+            //時間屬性
+            var duration = $(this).attr("data-goto-duration");
+            // 上方位置 = 目標屬性.位移.上方
+            var top = $(target).offset().top;
+            // 動畫({捲動上方：上方位置}，時間)
+            $("html, body").stop().animate({ scrollTop: top }, parseInt(duration));
+        }
+    });
+});
+
+// 當捲動滾輪時停止動畫
+// jqon 事件：mousewheel 滑鼠滾動
+
+$(window).on("mousewheel", function () {
+    $("*").each(function () {
+        $(this).stop();
+    });
+});
